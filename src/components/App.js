11c96e02a1bf746e805b1b4cd0017433c692f44f
araarq <aaq8@njit.edu>
2019-05-12 19:38:58 -0400

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
   }
 
   getData() {
-    axios.get('http://my-json-server.typicode.com/bnissen24/project2DB/posts')
+    axios.get('http://my-json-server.typicode.com/bnissen24/project2DB/accounts')
       .then(response => {
         this.props.setTasks(response.data);
       }).catch(error => {
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -3,12 +3,26 @@ import React from 'react';
 const TaskItem = props => {
   return (
     <li className="list-group-item">
-      { props.task.title }
+      Name:
+        { props.task.name }
+        <br></br>
+        Balance:
+        { props.task.balance }
       <button type="button"
               onClick={() => props.markDone(props.task)}
               className="btn btn-primary" style={{ float: 'right' }}>
         Done
       </button>
+        <button type="button"
+                onClick={() => props.markDone(props.task)}
+                className="btn btn-primary" style={{ float: 'right' }}>
+            Done
+        </button>
+        <button type="button"
+                onClick={() => props.markDone(props.task)}
+                className="btn btn-primary" style={{ float: 'right' }}>
+            test
+        </button>
     </li>
   )
 };
